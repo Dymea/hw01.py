297574e2eddc242396023981ed289bfb453dbca1
Dymea <dxs5940@psu.edu>
2020-09-04 19:01:54 +0000

    changed if to elif and added else

--- main.py
+++ main.py
@@ -9,33 +9,33 @@ credit1 = int(credit1)
 if  grade1 == 'A':
   grade1 = float(4.0)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == 'A-':
+elif grade1 == 'A-':
   grade1 = float(3.67)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == 'B+':
+elif grade1 == 'B+':
   grade1 = float(3.33)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == "B":
+elif grade1 == "B":
   grade1 = float(3.0)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == "B-":
+elif grade1 == "B-":
   grade1 = float(2.67)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == 'C+':
+elif grade1 == 'C+':
   grade1 = float(2.33)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == 'C':
+elif grade1 == 'C':
   grade1 = float(2.0)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == 'D':
+elif grade1 == 'D':
   grade1 = float(1.0)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 == 'F':
+elif grade1 == 'F':
   grade1 = float(0.0)
   print(f"Grade point for course 1 is: {grade1} ")
-if grade1 != 'A' or grade1 != 'A-' or grade1 != 'B+' or grade1 != 'B' or grade1 != 'B-' or grade1 != 'C+' or grade1 != 'C' or grade1 != 'D' or grade1 != 'F':
+else:
   grade1 = float(0.0)
-  print(f"Grade point for course 3 is: {grade1} ")
+  print(f"Grade point for course 1 is: {grade1} ")
 
 # User input for course 2
 grade2 = input("Enter your course 2 letter grade: ")
@@ -46,33 +46,33 @@ credit2 = int(credit2)
 if grade2 == 'A':
   grade2 = float(4.0)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == 'A-':
+elif grade2 == 'A-':
   grade2 = float(3.67)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == 'B+':
+elif grade2 == 'B+':
   grade2 = float(3.33)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == "B":
+elif grade2 == "B":
   grade2 = float(3.0)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == "B-":
+elif grade2 == "B-":
   grade2 = float(2.67)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == 'C+':
+elif grade2 == 'C+':
   grade2 = float(2.33)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == 'C':
+elif grade2 == 'C':
   grade2 = float(2.0)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == 'D':
+elif grade2 == 'D':
   grade2 = float(1.0)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 == 'F':
+elif grade2 == 'F':
   grade2 = float(0.0)
   print(f"Grade point for course 2 is: {grade2} ")
-if grade2 != 'A' or grade2 != 'A-' or grade2 != 'B+' or grade2 != 'B' or grade2 != 'B-' or grade2 != 'C+' or grade2 != 'C' or grade2 != 'D' or grade2 != 'F':
+else:
   grade2 = float(0.0)
-  print(f"Grade point for course 3 is: {grade2} ")
+  print(f"Grade point for course 2 is: {grade2} ")
 
 # User input for course 3
 grade3 = input("Enter your course 3 letter grade: ")
@@ -83,31 +83,31 @@ credit3 = int(credit3)
 if grade3 == 'A':
   grade3 = float(4.0)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == 'A-':
+elif grade3 == 'A-':
   grade3 = float(3.67)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == 'B+':
+elif grade3 == 'B+':
   grade3 = float(3.33)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == "B":
+elif grade3 == "B":
   grade3 = float(3.0)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == "B-":
+elif grade3 == "B-":
   grade3 = float(2.67)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == 'C+':
+elif grade3 == 'C+':
   grade3 = float(2.33)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == 'C':
+elif grade3 == 'C':
   grade3 = float(2.0)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == 'D':
+elif grade3 == 'D':
   grade3 = float(1.0)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 == 'F':
+elif grade3 == 'F':
   grade3 = float(0.0)
   print(f"Grade point for course 3 is: {grade3} ")
-if grade3 != 'A' or grade3 != 'A-' or grade3 != 'B+' or grade3 != 'B' or grade3 != 'B-' or grade3 != 'C+' or grade3 != 'C' or grade3 != 'D' or grade3 != 'F':
+else:
   grade3 = float(0.0)
   print(f"Grade point for course 3 is: {grade3} ")
 
